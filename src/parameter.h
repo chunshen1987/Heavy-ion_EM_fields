51b68cc8ccba5d02412923104a4a6078ba6bde22
Chun Shen <chunshen1987@gmail.com>
2016-04-01 11:32:30 -0400

    some clean up

--- src/parameter.h
+++ src/parameter.h
@@ -1,11 +1,9 @@
-#ifndef PARAMETER_H
-#define PARAMETER_H
-
-#include <string>
-using namespace std;
+// Copyright 2016 Chun Shen
+#ifndef SRC_PARAMETER_H_
+#define SRC_PARAMETER_H_
 
 const double alpha_EM = 1./137.;
-const double hbarC = 0.197327053;  // GeV*fm
-const double unit_convert = hbarC*hbarC; // convert 1/(fm^2) to GeV^2
+const double hbarC = 0.197327053;           // GeV*fm
+const double unit_convert = hbarC*hbarC;    // convert 1/(fm^2) to GeV^2
 
-#endif
+#endif  // SRC_PARAMETER_H_
